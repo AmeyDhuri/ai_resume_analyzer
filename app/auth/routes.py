eb7af4a3941f7c116ff634b1417a9622f45a4e14
Amey Dhuri <ameydhuri08@gmail.com>
2026-05-11 15:14:01 +0530

    user authentication

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -3,7 +3,7 @@ import logging
 from flask import Blueprint, request, jsonify
 from app.extensions import db
 from app.auth.models import User
-from app.auth.service import create_user
+from app.auth.service import create_user, authenticate_user
 
 auth_bp = Blueprint("auth", __name__)
 
@@ -40,7 +40,44 @@ def register():
     return jsonify({
         "success":True,
         "data": {
-            "email": email
+            "email": user.email
         },
         "message": "User successfully registered."
     }), 201
+
+@auth_bp.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email or not password:
+        return jsonify({"error": "Email and password required!"}), 400
+    
+    try:
+        user = authenticate_user(email, password)
+
+        if not user:
+            logging.warning("Failed to login for %s", email)
+            return jsonify({
+                "success" : False,
+                "message" : "Invalid email or pssword"
+            }), 401
+        
+        logging.info("Use logged in %s", email)
+
+        return jsonify({
+            "success" : True,
+            "data" :{
+                "email" : user.email
+            },
+            "message" : "Login successful"
+        }), 200
+    
+    except Exception as e:
+        logging.error("Login error for %s : %s", email, str(e))
+
+        return jsonify({
+            "error" : "Internal server error"
+        }), 500
--- app/auth/service.py
+++ app/auth/service.py
@@ -1,6 +1,6 @@
 from app.extensions import db
 from app.auth.models import User
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 def create_user(email, password):
@@ -15,4 +15,16 @@ def create_user(email, password):
     db.session.add(user)
     db.session.commit()
 
+    return user
+
+def authenticate_user(email, password):
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        return None
+    
+    if not check_password_hash(user.password, password):
+        return None
+    
     return user
